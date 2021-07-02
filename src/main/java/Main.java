301feeae778a6ca13ab777ead9252b75280693ff
Kogut <koguts@yandex.ru>
2021-07-02 18:03:39 +0300

    одиннадцатый коммит

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,5 +6,6 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         System.out.println("First");
+        System.out.println("args = " + Arrays.deepToString(args));
     }
 }
