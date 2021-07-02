3e8c5fd66b5c4c5eb4a117b6a1fddefb364fd5e4
Kogut <koguts@yandex.ru>
2021-07-02 20:03:12 +0300

    Еще одно изменение во второй ветке

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         System.out.println("First");
-        System.out.println("args = ");
+        System.out.println("args= ");
         System.out.println("Changes second branch 2");
     }
 }
