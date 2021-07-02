0237040a53a44d7dc513965a3fe0541eeaa156c5
Kogut <koguts@yandex.ru>
2021-07-02 18:03:01 +0300

    десятый коммит

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
+import java.util.Arrays;
+
 /**
  * @author S.Kogut on 30.06.2021
  */
 public class Main {
     public static void main(String[] args) {
         System.out.println("First");
-        System.out.println("Second");
     }
 }
