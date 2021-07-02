65cfbfaf3cc83c145bde3ef8a8e4ed003060b083
Kogut <koguts@yandex.ru>
2021-07-02 16:17:19 +0300

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,6 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("First");
-        System.out.printf("Second");
-        System.out.printf("Third");
+        System.out.println("First");
     }
 }
