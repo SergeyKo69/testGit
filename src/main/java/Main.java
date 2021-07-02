ce457031457f45bfb243d3d919da4bab02d0d394
Kogut <koguts@yandex.ru>
2021-07-02 19:58:06 +0300

    Commit in second branche

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 /**
  * @author S.Kogut on 30.06.2021
@@ -6,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First");
         System.out.println("args = ");
+        System.out.println("Changes second branch");
     }
 }
