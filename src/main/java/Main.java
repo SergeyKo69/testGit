ba0fc8ed9d473d69ab591276e6f452aff50bd40a
Kogut <koguts@yandex.ru>
2021-09-17 19:18:06 +0300

    third commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("first commit!");
         System.out.println("second commit");
+        System.out.println("third commit");
     }
 }
