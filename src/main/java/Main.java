a69f74d269a884db4a696e416f33f331c6f20699
Kogut <koguts@yandex.ru>
2021-09-17 19:12:45 +0300

    add extra commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("first commit");
+        System.out.println("first commit!");
+        System.out.println("second commit");
     }
 }
