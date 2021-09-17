24164e3ab9e5c31b906bb1328e0476d2ecba3d10
Kogut <koguts@yandex.ru>
2021-09-17 19:09:37 +0300

    init commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,6 @@ import java.util.Arrays;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("First");
-        System.out.println("args = ");
-        System.out.println("Changes second branch");
+
     }
 }
