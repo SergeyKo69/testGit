dcd430f1f07d47776d68931338e6a18942ef636f
Kogut <koguts@yandex.ru>
2021-09-17 19:13:45 +0300

    master change commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("first commit");
+        System.out.println("master commit");
+        System.out.println("master commit 2");
     }
 }
